8bcae825184a2cd430d8dee84ddda7f744944abd
antor100655gh <creativefreelancerantor@gmail.com>
2023-10-28 11:26:05 -0700

    add some change on product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -34,7 +34,7 @@ module.exports.getByFiltering = async (req, res) => {
     res.status(200).json(products);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).send('Internal server error');
   }
 }
 
@@ -58,12 +58,13 @@ module.exports.findByCategory = async (req, res) => {
     if (products.length === 0) {
       return res
         .status(404)
-        .json({ message: "No products found for this category." });
+        .send("No products found for this category.");
     }
 
     res.status(200).json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
+    res.status(500).send("Internal server error");
   }
 };
 
