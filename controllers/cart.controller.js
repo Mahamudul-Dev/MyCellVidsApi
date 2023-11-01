fdc64b4b3a79244997ab9aa1c96c01df997dc47b
antor100655gh <creativefreelancerantor@gmail.com>
2023-11-01 13:36:45 -0700

    add some change on cart option

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,11 +1,13 @@
 const Carts = require("../models/Carts");
 const Products = require("../models/Products");
+const Users = require("../models/Users");
 
 module.exports.addItemToCart = async (req, res) => {
     try {
         const getUserId = req.userId;
         const getProductId = req.body.productId;
 
+        const getUserDetails = await Users.findOne({ _id: getUserId });
         const cart = await Carts.findOne({ userId: getUserId });
         const product = await Products.findOne({ _id: getProductId });
 
@@ -25,12 +27,14 @@ module.exports.addItemToCart = async (req, res) => {
                 items: [productDetails],
             })
             await newCart.save();
+            await getUserDetails.updateOne({ $inc: { cartItemCount: 1 } })
             res.status(200).send(newCart);
         }
 
         if (cart) {
             cart.items.push(productDetails);
             await cart.save();
+            await getUserDetails.updateOne({ $inc: { cartItemCount: 1 } })
             res.status(200).send(cart);
         }
 
