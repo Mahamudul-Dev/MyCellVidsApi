ea234123fc22884f0eb9961986d5bd6b091cc25d
antor10655gh <creativefreelancerantor@gmail.com>
2023-11-18 10:59:19 -0800

    add change price in number

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const url = `mongodb+srv://mahamuduldev:0308412@my-cell-vids-db.8pf3ybb.mongodb.net/?retryWrites=true&w=majority`;
-// const url = "mongodb://localhost:27017";
+// const url = `mongodb+srv://mahamuduldev:0308412@my-cell-vids-db.8pf3ybb.mongodb.net/?retryWrites=true&w=majority`;
+const url = "mongodb://localhost:27017";
 
 mongoose
   .connect(url, {
