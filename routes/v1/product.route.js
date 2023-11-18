06f39bb9b733d8b92764f9a4b03a6460c6415c24
antor10655gh <creativefreelancerantor@gmail.com>
2023-11-18 02:15:56 -0800

    fix the getFiltering api

--- routes/v1/product.route.js
+++ routes/v1/product.route.js
@@ -5,10 +5,10 @@ const productController = require('../../controllers/product.controller');
 const { auth } = require('../../middleware/auth');
 
 app.get('/', auth, productController.allProducts);
-app.get('/:id', auth, productController.singleProduct);
 app.get('/allProducts', auth, productController.getByFiltering);
 app.get('/search', auth, productController.getBySearch);
 app.get('/searchByCategory', auth, productController.searchByCategory);
+app.get('/:id', auth, productController.singleProduct);
 app.get('/:id', auth, productController.singleProduct);
 app.get('/category/:category', auth, productController.findByCategory);
 app.post('/', auth, upload.single('productImg'), productController.addProduct);
