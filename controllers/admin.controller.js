f13492c713a7ca3d3047bca183eaa7e481b901c1
antor10655gh <creativefreelancerantor@gmail.com>
2023-11-29 16:04:17 +0600

    add some change

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,9 +1,8 @@
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const Admin = require("../models/Admin");
 
 const JWT_SECRET_KEY = "secret123";
-
 
 module.exports.createAdmin = async (req, res) => {
   try {
@@ -12,7 +11,9 @@ module.exports.createAdmin = async (req, res) => {
     // Check if an admin with the same email already exists
     const existingAdmin = await Admin.findOne({ email });
     if (existingAdmin) {
-      return res.status(409).json({ message: 'Admin with this email already exists' });
+      return res
+        .status(409)
+        .json({ message: "Admin with this email already exists" });
     }
 
     // Hash the password
@@ -25,12 +26,19 @@ module.exports.createAdmin = async (req, res) => {
       password: hashedPassword,
     });
 
-    const token = jwt.sign({ email: newAdmin.email, id: newAdmin._id }, JWT_SECRET_KEY);
-    return res.status(201).json({ message: 'Admin created successfully', admin: newAdmin, token });
+    const token = jwt.sign(
+      { email: newAdmin.email, id: newAdmin._id },
+      JWT_SECRET_KEY
+    );
+    return res
+      .status(201)
+      .json({ message: "Admin created successfully", admin: newAdmin, token });
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
-}
+};
 
 module.exports.adminLogin = async (req, res) => {
   try {
@@ -41,7 +49,7 @@ module.exports.adminLogin = async (req, res) => {
 
     // If the admin is not found
     if (!admin) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     // Compare the provided password with the hashed password in the database
@@ -49,27 +57,33 @@ module.exports.adminLogin = async (req, res) => {
 
     // If the password is invalid
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     // Generate a JWT token
-    const token = jwt.sign({ adminId: admin._id }, JWT_SECRET_KEY, { expiresIn: '1h' });
+    const token = jwt.sign({ adminId: admin._id }, JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
 
     // Return the token to the client
-    return res.status(200).json({ message: 'Login successful', admin, token });
+    return res.status(200).json({ message: "Login successful", admin, token });
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
-}
+};
 
 module.exports.getAdmin = async (req, res) => {
   try {
-    const admins = await Admin.findOne({});
-    return res.status(200).json({ message: 'Admins fetched successfully', admins: admins });
+    const admins = await Admin.find({});
+    return res.status(200).json(admins);
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
-}
+};
 
 module.exports.updatedAdmin = async (req, res) => {
   try {
@@ -80,7 +94,7 @@ module.exports.updatedAdmin = async (req, res) => {
     const adminToUpdate = await Admin.findById({ _id: adminId });
 
     if (!adminToUpdate) {
-      return res.status(404).json({ message: 'Admin not found' });
+      return res.status(404).json({ message: "Admin not found" });
     }
 
     // Hash the new password if provided
@@ -96,12 +110,13 @@ module.exports.updatedAdmin = async (req, res) => {
 
     await adminToUpdate.save();
 
-    return res.status(200).json({ message: 'Admin updated successfully', admin: adminToUpdate });
+    return res
+      .status(200)
+      .json({ message: "Admin updated successfully", admin: adminToUpdate });
   } catch (error) {
     console.log("Error", error);
-    return res.status(500).json({ message: 'Internal server error', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
-}
-
-
-
+};
