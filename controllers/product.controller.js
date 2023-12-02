ebad527a392e56e7f07a772baa1b8b86b028792d
antor10655gh <creativefreelancerantor@gmail.com>
2023-12-03 00:02:02 +0600

    add some change

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -289,11 +289,15 @@ module.exports.actionProduct = async (req, res) => {
 
     product.videoStatus = videoStatus;
 
-    if (videoStrike) {
-      product.videoStrike.push(videoStrike);
-      await product.save();
-    } else {
-      product.videoStrike = [];
+    if (videoStrike !== undefined) {
+      // Add or push videoStrike to product.videoStrike
+      if (!product.videoStrike) {
+        // If product.videoStrike is not an array, initialize it as an empty array
+        product.videoStrike = [videoStrike];
+      } else {
+        // If product.videoStrike is an array, push videoStrike to it
+        product.videoStrike.push(videoStrike);
+      }
     }
 
     await product.save();
