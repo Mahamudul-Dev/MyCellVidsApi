f23f8e5f59e2f45f7baf3105a31d062c2fcaa4fd
antor10655gh <creativefreelancerantor@gmail.com>
2023-12-19 22:59:54 +0600

    add getConverstion

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,4 +1,5 @@
 const Messages = require("../models/Messages");
+const Users = require("../models/Users");
 const { ObjectId } = require("mongoose").Types;
 
 module.exports.getMessages = async (req, res, next) => {
@@ -32,9 +33,50 @@ module.exports.getAllConversations = async (req, res, next) => {
           $or: [{ senderId: userId }, { receiverId: userId }],
         },
       },
+      {
+        $sort: {
+          createdAt: -1, // Sort in descending order based on message creation time
+        },
+      },
+      {
+        $group: {
+          _id: {
+            conversationId: {
+              $cond: {
+                if: { $eq: ["$senderId", userId] },
+                then: "$receiverId",
+                else: "$senderId",
+              },
+            },
+          },
+          lastMessage: { $first: "$$ROOT" }, // Get the first message in each group (i.e., the last message)
+        },
+      },
+      {
+        $replaceRoot: { newRoot: "$lastMessage" }, // Replace the root document with the last message
+      },
     ]);
 
-    res.json({ conversations: conversations });
+    const inbox = conversations.map((conversation) => {
+      return {
+        _id: conversation._id,
+        participants: {
+          participant1: conversation.senderId,
+          participant2: conversation.receiverId,
+        },
+        lastMessage: {
+          senderId: conversation.senderId,
+          receiverId: conversation.receiverId,
+          message: conversation.message,
+          createdAt: conversation.createdAt,
+        },
+      };
+    });
+
+    res.json({
+      message: "Data Fetch Successfully",
+      inbox: inbox,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while fetching messages");
